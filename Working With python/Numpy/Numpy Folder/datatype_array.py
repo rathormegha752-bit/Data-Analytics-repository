ade263673c25135a9e5c37da41633e540010887b
Megha Rathor <rathormegha752@gmail.com>
2026-02-05 16:49:52 +0530

    Delete Working With python/Numpy/Numpy Folder directory

--- Working With python/Numpy/Numpy Folder/datatype_array.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-#chnage the data type int into float
-arr = np.array([1, 2, 3], dtype=float)
-print(arr.dtype)
-
-#change datatype float into int
-arr1 = np.array([10.2,30.0,50.6,23.5], dtype=int)
-print(arr1.dtype)
--- Working With python/Numpy/Numpy Folder/fancy_indexing.py
+++
@@ -1,6 +0,0 @@
-# fancy indexing: use for select multiple element
-# selecting multiple element at once
-# it is used in list, it is create a copy
-import numpy as np
-arr = np.array([10,20,30,40,50])
-print(arr[[0,2,4]])
